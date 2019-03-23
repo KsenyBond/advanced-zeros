531e1505e692c91294659bee9a27c32f79a6004c
KsenyBond <kseny.bondarchuk@gmail.com>
2019-03-24 02:57:54 +0300

    feat: advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+
+  var i = 2;
+  var simpleNumbersFromBase = [],
+      summArr = [],
+      delimetr = 1;
+
+  while (i < base) {
+    if (base % i == 0) {
+      simpleNumbersFromBase.push(i);
+      base = base / i;
+    } else {
+      i++;
+    }
+  }
+  simpleNumbersFromBase.push(i);
+
+  simpleNumbersFromBase.forEach(function(value, index, arr){
+      var valueDegree = 1,
+        summ = 0;
+
+    if (simpleNumbersFromBase.length >= (index+1) && arr[index +1] != value) {
+      while((number/Math.pow(value, valueDegree)) > 1) {
+        summ = summ + Math.floor(number/Math.pow(value, valueDegree))/delimetr;
+        valueDegree++;
+
+      }
+      delimetr = 1;
+    } else {
+      delimetr++;
+    }
+
+    if (summ > 0) {
+      summArr.push(Math.floor(summ));
+    }
+  });
+
+  return Math.min.apply(null, summArr);
+};
